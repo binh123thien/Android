0c8dbdabfb78d018bfdf6aad6abed3fcecf26736
binh123thien <100462512+binh123thien@users.noreply.github.com>
2022-11-19 08:27:53 +0700

    ko
    project CK

---
+++ app/src/main/java/com/example/project_cuoi_ki/Dashboard.java
@@ -0,0 +1,71 @@
+package com.example.project_cuoi_ki;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+    ImageView imageliving,imagebed,imagekitchen;
+    TextView textliving,textbed,textkitchen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+        imageliving = findViewById(R.id.imagelivingroom);
+        imagebed = findViewById(R.id.imagebedroom);
+        imagekitchen = findViewById(R.id.imagekitchenroom);
+        textliving = findViewById(R.id.textlivingroom);
+        textbed = findViewById(R.id.textbedroom);
+        textkitchen = findViewById(R.id.textkitchenroom);
+
+
+//        // nút nhấn để chuyển tới giao diện living room
+//        imageliving.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent=new Intent(menu.this,livingroom.class);
+//                startActivity(intent);
+//            }
+//        });
+//        textliving.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent=new Intent(menu.this,livingroom.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        //nut nhấn để chuyển tới bedroom
+//        imagebed.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent=new Intent(menu.this,bedroom.class);
+//                startActivity(intent);
+//            }
+//        });
+//        textbed.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent=new Intent(menu.this,bedroom.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        //nút nhấn để chuyển tới kitchen room
+//        imagekitchen.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent=new Intent(menu.this,kitchenroom.class);
+//                startActivity(intent);
+//            }
+//        });
+//        textkitchen.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent=new Intent(menu.this,kitchenroom.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/project_cuoi_ki/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.project_cuoi_ki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        test = findViewById(R.id.textView);
+    }
+}
