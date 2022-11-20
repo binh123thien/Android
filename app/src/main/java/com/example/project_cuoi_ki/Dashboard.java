79cf95defe8119e76c776df94a4446853f2464a2
2k1QuangLinh <118100679+2k1QuangLinh@users.noreply.github.com>
2022-11-20 16:25:38 +0700

    gg

--- app/src/main/java/com/example/project_cuoi_ki/Dashboard.java
+++ app/src/main/java/com/example/project_cuoi_ki/Dashboard.java
@@ -3,19 +3,28 @@ package com.example.project_cuoi_ki;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Dashboard extends AppCompatActivity {
     ImageView imageliving,imagebed,imagekitchen;
-    TextView textliving,textbed,textkitchen;
+    TextView textliving,textbed,textkitchen,textnhietdo,textdoam;
     TextClock time;
     private static final String TIME_FORMAT_24 = "HH:mm:ss";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +36,44 @@ public class Dashboard extends AppCompatActivity {
         textbed = findViewById(R.id.textbedroom);
         textkitchen = findViewById(R.id.textkitchenroom);
         time=findViewById(R.id.texttime);
-
+        textnhietdo = findViewById(R.id.nhiet_do);
+        textdoam = findViewById(R.id.do_am);
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
         // hiển thị thòi gian
         String formatdate = "E,   d-M-yyyy    hh:mm:ss";
         time.setFormat24Hour(formatdate);
 
+        //đọc nhiệt độ, độ ẩm
+        final DatabaseReference nhietdo=database.getReference("NhietDo");
+        final DatabaseReference doam=database.getReference("DoAm");
+
+        nhietdo.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue(String.class);
+                value = value + "°C";
+                textnhietdo.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        doam.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value=dataSnapshot.getValue(String.class);
+                value = value + "%";
+                textdoam.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         // nút nhấn để chuyển tới giao diện living room
         imageliving.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
--- app/src/main/java/com/example/project_cuoi_ki/Living_Room.java
+++ app/src/main/java/com/example/project_cuoi_ki/Living_Room.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Living_Room extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.livingroom);
+
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+
+        final DatabaseReference nhietdo=database.getReference("NhietDo");
     }
 }
