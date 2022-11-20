34cec41565843d8056f2a7ebefd9cf8a208397dc
nhatkhangcao <caonhatkhang2001@gmail.com>
2022-11-20 16:44:11 +0700

    hi

--- app/src/main/java/com/example/project_cuoi_ki/Dashboard.java
+++ app/src/main/java/com/example/project_cuoi_ki/Dashboard.java
@@ -2,16 +2,25 @@ package com.example.project_cuoi_ki;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Dashboard extends AppCompatActivity {
+    AlertDialog.Builder builderDialog;
+    AlertDialog alertDialog;
     ImageView imageliving,imagebed,imagekitchen;
     TextView textliving,textbed,textkitchen;
     TextClock time;
@@ -27,6 +36,45 @@ public class Dashboard extends AppCompatActivity {
         textbed = findViewById(R.id.textbedroom);
         textkitchen = findViewById(R.id.textkitchenroom);
         time=findViewById(R.id.texttime);
+
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homeaction:
+                        Toast.makeText(Dashboard.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.personaction:
+                        Toast.makeText(Dashboard.this, "Alo", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.logoutaction:
+                        showAlertDialog(R.layout.dialog);
+                        break;
+                        
+                }
+                return true;
+            }
+
+            private void showAlertDialog(int dialog) {
+                builderDialog = new AlertDialog.Builder(Dashboard.this);
+                View layoutView = getLayoutInflater().inflate(dialog,null);
+                Button btnlogout = layoutView.findViewById(R.id.btnlogout);
+
+                builderDialog.setView(layoutView);
+                alertDialog = builderDialog.create();
+                alertDialog.show();
+                btnlogout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+            }
+        });
+
 
         // hiển thị thòi gian
         String formatdate = "E,   d-M-yyyy    hh:mm:ss";
--- app/src/main/java/com/example/project_cuoi_ki/Kitchen_Room.java
+++ app/src/main/java/com/example/project_cuoi_ki/Kitchen_Room.java
@@ -1,13 +1,39 @@
 package com.example.project_cuoi_ki;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Kitchen_Room extends AppCompatActivity {
+    TextView tv_seekbarfridge;
+    SeekBar seekbarfridge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kitchenroom);
+        init();
+        seekbarfridge.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tv_seekbarfridge.setText(""+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    private void init(){
+        tv_seekbarfridge = findViewById(R.id.tv_seekbarfridge);
+        seekbarfridge = findViewById(R.id.seekBarfridge);
     }
 }
