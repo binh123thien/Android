32a0f9ada004247e0be9f4ba654639514e514194
2k1QuangLinh <118100679+2k1QuangLinh@users.noreply.github.com>
2022-11-20 17:18:07 +0700

    ff

--- app/src/main/java/com/example/project_cuoi_ki/Living_Room.java
+++ app/src/main/java/com/example/project_cuoi_ki/Living_Room.java
@@ -1,6 +1,8 @@
 package com.example.project_cuoi_ki;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +10,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Living_Room extends AppCompatActivity {
-    
+    ImageView TVicon,Lampicon,Camaraicon;
+    Switch swTV, swLamp, swCamera;
+    boolean stateTV,stateLamp,stateCamara
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
